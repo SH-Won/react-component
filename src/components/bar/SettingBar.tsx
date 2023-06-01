88105e5de8a4ca85efb0c6edc65d88816acb465f
SH-Won <1317good@naver.com>
2023-06-01 12:00:17 +0900

    #41 set props in order to recycle

--- src/components/bar/SettingBar.tsx
+++ src/components/bar/SettingBar.tsx
@@ -1,121 +1,83 @@
 import { useLayoutEffect, useRef, useState } from 'react'
 import './styles/SettingBar.scss'
 
-const items = [
-  {
-    id: 0,
-    name: 'start',
-  },
-  {
-    id: 1,
-    name: 'middle',
-  },
-  {
-    id: 2,
-    name: 'end',
-  },
-  {
-    id: 3,
-    name: 'total',
-  },
-  {
-    id: 4,
-    name: 'ttoal',
-  },
-  {
-    id: 5,
-    name: 'totalll',
-  },
-  {
-    id: 6,
-    name: 'ttoalllll',
-  },
-  {
-    id: 7,
-    name: 'ttoaldasdf',
-  },
-  {
-    id: 8,
-    name: 'totallld',
-  },
-  {
-    id: 9,
-    name: 'ttoallllldd',
-  },
-]
-interface SettingBarProps<T> {
-  items: T[]
-  onSelect: () => void
+interface Item {
+  key?: string
+  value: string
+  order?: number
 }
-const SettingBar = () => {
-  const [position, setPosition] = useState<(typeof items)[0]>(items[0])
-  const onClickProgress = (item: (typeof items)[0]) => {
+interface SettingBarProps {
+  items: Item[]
+  width?: number // px % ??
+  onSelect?: (item: Item) => void
+}
+const SettingBar = ({ items, width, onSelect }: SettingBarProps) => {
+  const convertedItems = items?.map((item, order) => ({
+    ...item,
+    order,
+    key: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(),
+  }))
+  const itemLength = items.length
+  const [size, setSize] = useState<number>(0)
+  const [position, setPosition] = useState<Item>(convertedItems[0])
+  const onClickProgress = (item: Item) => {
     setPosition(item)
+    onSelect?.(item)
   }
-  const gap = useRef<number>()
   const bar = useRef<HTMLDivElement>(null)
   const calcWidth = () => {
-    let width
-    const itemLength = items.length - 1
     const barWidth = bar.current?.getBoundingClientRect().width as number
-    const averagePercent = Math.floor(100 / itemLength)
-    // const averageWidth = barWidth / itemLength
-    width = `${averagePercent * position.id}%`
-    if (position.id !== 0 && position.id !== itemLength) {
-      if (position.id === Math.floor(items.length / 2)) {
-      } else if (position.id < Math.floor(items.length / 2)) {
-        width += ` + 7px`
-      } else {
-        width += ` - 7px`
-      }
+    const gap = (barWidth - items.length * size) / (items.length - 1)
+    let calc = gap * position.order! + size * position.order!
+    if (position.order !== 0) {
+      calc += size / 2
     }
-    console.log(width)
-    const middle = Math.floor(items.length / 2)
-    // if (position.id !== 0 && position.id !== itemLength) {
-    //   width +=
-    //     position.id < middle
-    //       ? ` + ${gap.current! * (middle - position.id - 1)}px`
-    //       : ` - ${gap.current! * (position.id - middle)}px`
-    //   console.log(width)
-    // }
     return {
-      width: `calc(${width})`,
-      // width: `${averageWidth * position.id}px`,
+      width: `${calc}px`,
     }
   }
   useLayoutEffect(() => {
-    const barWidth = bar.current?.getBoundingClientRect().width as number
-    gap.current = (barWidth - items.length * 30) / (items.length - 1)
-  }, [bar])
+    const barWidth = bar.current!.getBoundingClientRect().width
+    if (!width) {
+      setSize(barWidth / itemLength)
+    } else {
+      setSize(width / itemLength)
+    }
+  }, [width])
 
   return (
-    <div>
+    <div style={{ width, height: size }}>
       <div className="setting-bar" ref={bar}>
-        {items.map((item, index) => (
+        {convertedItems.map((item, index) => (
           <div
-            className={`progress ${item.id === position.id ? 'selected' : ''}`}
-            key={item.id + item.name}
+            className={`progress ${
+              item.order === position.order ? 'selected' : ''
+            }`}
+            key={item.key}
+            style={{ width: size }}
             onClick={() => onClickProgress(item)}
           >
             <div
-              className={`point ${item.id < position.id ? 'selected' : ''}`}
+              className={`point ${
+                item.order < position.order! ? 'selected' : ''
+              }`}
             ></div>
           </div>
         ))}
         <div
           className={`user-progress ${
-            position.id === items.length - 1 ? 'end' : ''
+            position.order === itemLength - 1 ? 'end' : ''
           }`}
           style={calcWidth()}
         ></div>
       </div>
       <div className="setting-explain">
-        {items.map((item, index) => (
+        {convertedItems.map((item, index) => (
           <span
-            key={item.name}
-            className={index === position.id ? 'selected' : ''}
+            key={item.key}
+            className={index === position.order ? 'selected' : ''}
           >
-            {item.id}
+            {item.order + 1}
           </span>
         ))}
       </div>
--- src/components/bar/stories/SettingBar.stories.tsx
+++ src/components/bar/stories/SettingBar.stories.tsx
@@ -2,7 +2,38 @@ import Colors from '../../../styles/colors.module.scss'
 import type { Meta, StoryObj } from '@storybook/react'
 import SettingBar from '../SettingBar'
 import Button from '../../button/Button'
-
+const items = [
+  {
+    value: 'start',
+  },
+  {
+    value: 'middle',
+  },
+  {
+    value: 'end',
+  },
+  {
+    value: 'total',
+  },
+  {
+    value: 'ttoal',
+  },
+  {
+    value: 'totalll',
+  },
+  {
+    value: 'ttoalllll',
+  },
+  {
+    value: 'ttoaldasdf',
+  },
+  {
+    value: 'totallld',
+  },
+  {
+    value: 'ttoallllldd',
+  },
+]
 const meta: Meta<typeof SettingBar> = {
   title: 'Bar',
   component: SettingBar,
@@ -10,4 +41,9 @@ const meta: Meta<typeof SettingBar> = {
 export default meta
 type Story = StoryObj<typeof SettingBar>
 
-export const Setting: Story = {}
+export const Setting: Story = {
+  args: {
+    width: 300,
+    items,
+  },
+}
